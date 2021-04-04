ff4bb4c751c52ccb4c1dd536700b55e9eccde088
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-04-04 18:07:14 -0400

    Integrated Firebase into app, created sign in with Google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyBPRAGNMZ4gc5g_SKlR5XEknN4u-fxnxPs",
+  authDomain: "zerotomastery-react.firebaseapp.com",
+  databaseURL: "https://zerotomastery-react.firebaseio.com",
+  projectId: "zerotomastery-react",
+  storageBucket: "zerotomastery-react.appspot.com",
+  messagingSenderId: "51885256246",
+  appId: "1:51885256246:web:f4188b1f1e0ecc66f37297",
+  measurementId: "G-XQ2143TZGT"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ promp: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
