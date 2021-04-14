009793fccee0f2527e71a8ce0c155f34ee25b636
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-04-14 18:19:39 -0400

    Added checkout page, checkout item component, and updates to cart dropdown

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,8 +16,15 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
+      (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (accumlatedTotal, cartItem) => accumlatedTotal + (cartItem.quantity * cartItem.price),
+    0
+  )
 );
