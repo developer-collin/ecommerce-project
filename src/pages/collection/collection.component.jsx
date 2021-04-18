20b759d61b5bdacd0730009914a9ac4d532ca8ea
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-04-18 16:28:17 -0400

    Changed shop data from array to object and updated corresponding reducers. Collection page updated to display items.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,22 @@
 import './collection.styles.scss';
 
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
+
   return (
     <div className='collection-page'>
-      <h2>COLLECTION PAGE</h2>
+      <h2 className='title'>{ title }</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   )
 };
