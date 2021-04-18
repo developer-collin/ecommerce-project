e93ae27d7c78f854b27b81a2745ac164a2cc12d1
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-04-18 15:12:06 -0400

    Stub out collection page and hook up with shop component, add lodash memoize for selectCollection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import './collection.styles.scss';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  return (
+    <div className='collection-page'>
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  )
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
