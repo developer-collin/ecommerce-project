2dddfb9330b9b431d207bfdd8dc9b2013f927c29
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-04-18 16:52:16 -0400

    Simplifying returning of values in selectCollectionsForPreview

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -11,8 +11,8 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
-)
+  collections => Object.values(collections)
+);
 
 export const selectCollection = memoize(collectionUrlParam =>
   createSelector(
