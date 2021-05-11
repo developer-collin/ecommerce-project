93834c2f632048da20dd4c77df97853e7d9953ca
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-05-11 14:02:32 -0400

    Introduce lazy loading to page components

---
+++ src/components/spinner/spinner.component.jsx
@@ -0,0 +1,12 @@
+import {
+  SpinnerOverlay,
+  SpinnerContainer
+} from './spinner.styles';
+
+const Spinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
+
+export default Spinner;
