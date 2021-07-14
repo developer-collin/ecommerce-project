db79afa6b63b9b982eff4f84b8b408a3ed101b20
Collin <2819398-devcollin@users.noreply.gitlab.com>
2021-07-14 15:42:01 -0400

    Setting up environment variables

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+let config = functions.config().env;
+
+// const stripe = require('stripe')(functions.config().stripe.testsecret);
+// const express = require('express');
+// const app = express();
+// app.use(express.static('.'));
+// const YOUR_DOMAIN = 'http://localhost:3000/checkout';
 
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
@@ -16,3 +23,27 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Send back a message that we've successfully written the message
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
+
+// app.post('/create-checkout-session', async (req, res) => {
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ['card'],
+//     line_items: [
+//       {
+//         price_data: {
+//           currency: 'usd',
+//           product_data: {
+//             name: 'Stubborn Attachments',
+//             images: ['https://i.imgur.com/EHyR2nP.png'],
+//           },
+//           unit_amount: 2000,
+//         },
+//         quantity: 1,
+//       },
+//     ],
+//     mode: 'payment',
+//     success_url: `${YOUR_DOMAIN}?success=true`,
+//     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+//   });
+
+//   res.redirect(303, session.url)
+// });
